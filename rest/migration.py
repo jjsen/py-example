05867743a38482b4f87dace15a20ada205b6283f
jjsen <87337261+jjsen@users.noreply.github.com>
2023-10-10 20:11:54 +0200

    Update migration.py

--- rest/migration.py
+++ rest/migration.py
@@ -16,20 +16,44 @@ headers = {
     "Authorization": f"Basic {auth_encoded}"
 }
 
-def get_ticket_count(jira_url):
-    url = f"{jira_url}/rest/api/3/search"
+def get_remote_links(jira_url, project_key):
+    url = f"{jira_url}/rest/api/3/issue/{project_key}/remotelink?properties=*all"
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        print(f'Failed to retrieve data from {jira_url} for project {project_key}: {response.text}')
+        return None
+    return response.json()
+
+def compare_links(project_key):
+    source_links_json = get_remote_links(source_jira_url, project_key)
+    target_links_json = get_remote_links(target_jira_url, project_key)
+
+    if source_links_json is None or target_links_json is None:
+        return
+
+    source_keys = {link['key'] for link in source_links_json}
+    target_keys = {link['key'] for link in target_links_json}
+
+    missing_keys = source_keys - target_keys
+
+    print(f'Keys in source but not in target for project {project_key}: {missing_keys}')
+    print(f'Count of keys in source but not in target for project {project_key}: {len(missing_keys)}')
+
+def fetch_project_keys():
+    url = f"{source_jira_url}/rest/api/3/search"
     params = {
-        "jql": "",  
-        "maxResults": 0, 
+        "jql": "remotelinkscount>0",
+        "maxResults": 50,
+        "fields": "id,key"
     }
     response = requests.get(url, headers=headers, params=params)
     if response.status_code != 200:
-        print(f'Failed to retrieve data from {jira_url}: {response.text}')
-        return
+        print(f'Failed to retrieve project keys: {response.text}')
+        return []
 
-    total_tickets = response.json()['total']
-    print(f'Total number of tickets in {jira_url}: {total_tickets}')
+    issues_data = response.json()["issues"]
+    for issue in issues_data:
+        compare_links(issue['key'])
 
-# Execute the function to get the ticket count for both source and target URLs
-get_ticket_count(source_jira_url)
-get_ticket_count(target_jira_url)
+# Execute the function to fetch project keys and compare links
+fetch_project_keys()
