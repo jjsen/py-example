38b1831e3c735c7edc86b18cb5ff2071366c38f6
jjsen <87337261+jjsen@users.noreply.github.com>
2023-10-10 20:17:25 +0200

    Update migration.py

--- rest/migration.py
+++ rest/migration.py
@@ -57,3 +57,47 @@ def fetch_project_keys():
 
 # Execute the function to fetch project keys and compare links
 fetch_project_keys()
+
+
+
+
+
+
+
+
+
+
+def get_remote_links(jira_url, project_key):
+    url = f"{jira_url}/rest/api/3/issue/{project_key}/remotelink?properties=*all"
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        print(f'Failed to retrieve data from {jira_url} for project {project_key}: {response.text}')
+        return None
+    return response.json()
+
+def compare_links(project_key):
+    try:
+        source_links_json = get_remote_links(source_jira_url, project_key)
+        target_links_json = get_remote_links(target_jira_url, project_key)
+
+        if source_links_json is None or target_links_json is None:
+            return
+
+        source_keys = {link['key'] for link in source_links_json}
+        target_keys = {link['key'] for link in target_links_json}
+
+        missing_keys = source_keys - target_keys
+
+        print(f'Keys in source but not in target for project {project_key}: {missing_keys}')
+        print(f'Count of keys in source but not in target for project {project_key}: {len(missing_keys)}')
+    except KeyError as e:
+        print(f"KeyError: {e}")
+        print(f"Project Key: {project_key}")
+        print("Source JSON:")
+        print(json.dumps(source_links_json, indent=4))
+        print("Target JSON:")
+        print(json.dumps(target_links_json, indent=4))
+
+# ... rest of your code
+
+
